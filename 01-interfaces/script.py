6531c65f5fae411e4d28e5949a7a23dba5a211ed
Tobias Nguyen <tobias1001001@proton.me>
2025-09-20 15:37:51 +0200

    Added 01-interfaces

---
+++ 01-interfaces/script.py
@@ -0,0 +1,54 @@
+from abc import ABC, abstractmethod
+from math import pi
+
+# Geometry interface
+class Geometry(ABC):
+    @abstractmethod
+    def area(self) -> float:
+        pass
+
+    @abstractmethod
+    def perim(self) -> float:
+        pass
+
+# Rect class
+class Rect(Geometry):
+    def __init__(self, width: float, height: float):
+        self._width = width
+        self._height = height
+
+    def area(self) -> float:
+        return self._width * self._height
+
+    def perim(self) -> float:
+        return 2 * (self._width + self._height)
+
+# Circle class
+class Circle(Geometry):
+    def __init__(self, radius: float):
+        self._radius = radius
+
+    def area(self) -> float:
+        return pi * self._radius * self._radius
+
+    def perim(self) -> float:
+        return 2 * pi * self._radius
+    
+# Function to measure both area and perimeter
+def measure(g: Geometry):
+    print(g)
+    print(g.area())
+    print(g.perim())
+
+# Instantiate classes
+rect = Rect(
+    width = 3,
+    height = 4
+)
+
+circle = Circle(
+    radius = 5
+)
+
+measure(rect)
+measure(circle)
